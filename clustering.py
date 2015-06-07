1e59bb02e83770d8d7f1ad7d0f7c58c202341701
v123582 <v123582@gmail.com>
2015-06-08 01:46:39 +0800

    updata clustering

--- clustering.py
+++ clustering.py
@@ -1,20 +1,47 @@
 import numpy as np
 import pandas as pd
 import nltk
+import string
 import re
 import os
 import codecs
 from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
+stemmer = nltk.stem.porter.PorterStemmer()
+remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+
+def stem_tokens(tokens):
+    return [stemmer.stem(item) for item in tokens]
+
+'''remove punctuation, lowercase, stem'''
+def normalize(text):
+    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
+
+vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
+
+def cosine_sim(text1, text2):
+    tfidf = vectorizer.fit_transform([text1, text2])
+    return ((tfidf * tfidf.T).A)[0,1]
+
+a = 'We demonstrate the equivalence of Virasoro constraints imposed on continuumlimit of partition function of Hermitean 1 matrix model and the Ward identitiesof Kontsevich s model Since the first model describes ordinary d 2 quantumgravity while the second one is supposed to coincide with Witten s topologicalgravity the result provides a strong implication that the two models areindeed the same'
+
+
+
+
+
 authors = []
 contents = []
 
-with open("result", "r") as lines:
+with open("result_min", "r") as lines:
     for line in lines:
         authors.append(line.split(',')[0])
         contents.append(line.split(',')[1])
 
-print len(authors)
-print len(contents)
+#print len(authors)
+#print len(contents)
 
 stopwords = nltk.corpus.stopwords.words('english')
 #print stopwords[:10]
@@ -77,7 +104,7 @@ dist = 1 - cosine_similarity(tfidf_matrix)
 
 from sklearn.cluster import KMeans
 
-num_clusters = 300
+num_clusters = 5
 
 km = KMeans(n_clusters=num_clusters)
 
@@ -95,7 +122,7 @@ films = { 'authors': authors,'contents': contents, 'clusters': clusters }
 
 frame = pd.DataFrame(films, index = [clusters] , columns = [ 'authors', 'clusters', 'contents'])
 
-print frame['clusters'].value_counts()
+#print frame['clusters'].value_counts()
 
 
 
@@ -103,17 +130,31 @@ print frame['clusters'].value_counts()
 #print('----------')
 #sort cluster centers by proximity to centroid
 order_centroids = km.cluster_centers_.argsort()[:, ::-1] 
+maxSim = 0
+max_cluster = 0
+max_authors = []
 
-#for i in range(num_clusters):
-
+for i in range(num_clusters):
+    temp = ""
     #print("-----Cluster %d words:" % i)
-    #for ind in order_centroids[i, :10]: #replace 6 with n words per cluster
-        #print(' %s' % vocab_frame.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'))
+    for ind in order_centroids[i, :100]: #replace 6 with n words per cluster
+        temp += vocab_frame.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore')
+        temp += " "
+    #print(' %s' % temp)
+    #print a
+    #print cosine_sim(a, temp)
+    if cosine_sim(a, temp) > maxSim:
+        maxSim = cosine_sim(a, temp)
+        max_cluster = i
+        max_authors = frame.ix[i]['authors']
+
     
     #print("-----Cluster %d titles:" % i)
     #print(frame.ix[i]['authors'])
     
+#print max_cluster
+for max_author in max_authors:
+    for i in max_author.replace('Authors:', '').split('and'):
+        print i
+
 #print('----------')
-
-
-
